656d26883f79c367cf7c16596f19ecf1954fb50d
Paul Gaulmin <paul.gaulmin@ynov.com>
2026-09-01 17:24:25 +0200

    bug injection

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func Add(a, b int) int { return a + b }
 func Multiply(a, b int) int { return a * b }
+func Divide(a, b int) int { return a / b }
 
 func main() {
 	fmt.Println("Add(2, 3) =", Add(2, 3))
+	Divide(10, 0)
 }
