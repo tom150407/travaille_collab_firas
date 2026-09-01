629861002728e5f1c2a1384d8a4700750e835ecd
Paul Gaulmin <paul.gaulmin@ynov.com>
2026-09-01 17:31:32 +0200

    correction bug

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func Add(a, b int) int { return a + b }
 func Multiply(a, b int) int { return a * b }
-func Divide(a, b int) int { return a / b }
-
+func Divide(a, b int) (int, error) { if b == 0 { return 0, errors.New("division by zero") } return a / b, nil }
 func main() {
 	fmt.Println("Add(2, 3) =", Add(2, 3))
 	Divide(10, 0)
